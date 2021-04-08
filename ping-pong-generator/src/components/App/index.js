65fa7e22cbfaf799538c8f1921997d7697bc7e64
William Braney <williambraney@aol.com>
2021-04-08 15:15:26 +0100

    Updates

--- ping-pong-generator/src/components/App/index.js
+++
@@ -1,8 +0,0 @@
-import { connect } from "react-redux";
-import App from "./App";
-const mapStateToProps = state => {
-    return {
-        submitted: state.submitted
-    }
-}
-export default connect(mapStateToProps)(App);
